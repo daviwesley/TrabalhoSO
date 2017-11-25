0080f951c0aaf7aa4f33c5bde5c29259616c25c6
Davi Wesley <davi.wesley@icloud.com>
2017-11-25 19:00:21 -0200

    Update funcoesthreads.h

--- Trabalho1/funcoesthreads.h
+++ Trabalho1/funcoesthreads.h
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <time.h>
 void* funcao_thread1(void* unused);
 void* funcao_thread2(void* unused);
 void* funcao_thread3(void* unused);
+void esperar();
